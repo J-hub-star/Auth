99382aba207e76639b83982c6bdca437f9b76edf
J-hub-star <jaibehl33@gmail.com>
2021-05-17 21:07:49 -0500

    new push

--- src/main/java/com/example/trytwo/config/MongoDBConfig.java
+++ src/main/java/com/example/trytwo/config/MongoDBConfig.java
@@ -1,13 +1,20 @@
 package com.example.trytwo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
 public class MongoDBConfig extends AbstractMongoClientConfiguration
 {
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
     @Override
     protected String getDatabaseName() {
-        return null;
+        return this.database;
     }
 }
