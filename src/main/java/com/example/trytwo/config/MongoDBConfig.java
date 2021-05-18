4183045d30d09ef7756727e43dedc8688cd9a833
J-hub-star <jaibehl33@gmail.com>
2021-05-17 21:02:06 -0500

    new push

--- src/main/java/com/example/trytwo/config/MongoDBConfig.java
+++ src/main/java/com/example/trytwo/config/MongoDBConfig.java
@@ -1,4 +1,13 @@
 package com.example.trytwo.config;
 
-public class MongoDBConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class MongoDBConfig extends AbstractMongoClientConfiguration
+{
+    @Override
+    protected String getDatabaseName() {
+        return null;
+    }
 }
