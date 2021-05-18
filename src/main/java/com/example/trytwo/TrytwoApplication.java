994c35edf8268dc37e4afac6a1ba10a6b528f552
J-hub-star <jaibehl33@gmail.com>
2021-05-17 20:27:43 -0500

    Second

---
+++ src/main/java/com/example/trytwo/TrytwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.trytwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrytwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrytwoApplication.class, args);
+	}
+
+}
