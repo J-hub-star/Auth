14e0c4b4deae7ace27ce7b07eadc6673c7e8454b
2hotGrantPurple <evilgamer610@gmail.com>
2021-05-31 14:08:57 -0500

    Updated Service

--- src/main/java/com/bugtracker/springboot/service/ProjectTaskService.java
+++ src/main/java/com/bugtracker/springboot/service/ProjectTaskService.java
@@ -99,10 +99,6 @@ public class ProjectTaskService {
 
     public void deletePTByProjectSequence(String backlog_id, String pt_id){
         ProjectTasks projectTask = findPTByProjectSequence(backlog_id, pt_id);
-        Backlog backlog = projectTask.getBacklog();
-        List<ProjectTasks> pts = backlog.getProjectTasks();
-        pts.remove(projectTask);
-        backlogRepository.save(backlog);
         projectTaskRepository.delete(projectTask);
     }
 }
